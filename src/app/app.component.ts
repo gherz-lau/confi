d7073d018dbd8bb017988f5ce0a2d1ebe86d3d24
LauGonzalez <larry.gher@gmail.com>
2022-06-16 13:56:03 -0600

    3e commit -csv a array e imprime en consola - ya depurado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,242 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularcsv';
-
- //array varibales to store csv data
- lines: string[] = []; //ARREGLO PARA ENCABEZADOS
- linesR: string[]= []; // ARREGLO PARA REGISTROS OLINEAS
- file:any;  //DECLARAS VARIABLE FILE TIPO ANY
+  file:any;  //DECLARAS VARIABLE FILE TIPO ANY
  
- fileChanged(e:any) {  //ESTA ES LA FUNCION
-   this.file = e.target.files[0]; 
-
+  fileChanged(e:any) {  //ESTA ES LA FUNCION
+  
+  //CARGAMOS EL ARCHIVO 
+  this.file = e.target.files[0]; 
   let fileReader = new FileReader(); 
-   fileReader.onload = (e) => { 
-     let csv: any = fileReader.result;
-   let allTextLines = []; //DECLARAS UN ARREGLO
-   //allTextLines = csv.split(/\r|\n|\r/); //PARTES EL CSV Y LO PONES EN EL ARREGLO alltextlines
-   allTextLines = csv.split(/\r|\n|\r/);
-   
-   let longitud = allTextLines.length;
-
-   for(let i = 1; i < longitud; i++){
-    console.log("la", i, "posicion es", allTextLines[i].split(';'))
-   }
-   
-
-  
-  
-  
-   //Table Headings
-   let headers = allTextLines[0].split(';');
-   let data = headers;
-   let tarr: string[] | null | any = [];
-   
-   for (let j = 0; j < headers.length; j++) {
-     tarr.push(data[j]);
-   }
-   //Pusd headings to array variable
-   
-
-   this.lines.push(tarr);
-   
-  
-   // Table Rows
-   let tarrR: string[] | null | any = [];
-   let arrl = allTextLines.length;
-   let rows = [];
-   for(let i = 1; i < arrl; i++){
-   rows.push(allTextLines[i].split(';'));
-  }
-   
-   for (let j = 0; j < arrl; j++) {
-
-       tarrR.push(rows[j]);
-       console.log(rows[j])
-       
-   }
-  //Push rows to array variable
-   this.linesR.push(tarrR); 
-
-   
-    } 
-    fileReader.readAsText(this.file);
-
     
-  }
-
-  
-}
-/*
-
-if(file && files.length > 0) {
-  console.log(file.name);
-  console.log(file.size);
-  console.log(file.type);
-  //File reader method
-  let reader: FileReader = new FileReader();
-  reader.readAsText(file);
-  
-  reader.onload = (e) => {
-   let csv: any = reader.result;
-   let allTextLines = [];
-   allTextLines = csv.split(/\r|\n|\r/);
-  
-  //Table Headings
-   let headers = allTextLines[0].split(';');
-   let data = headers;
-   let tarr: string[] | null | any = [];
-   
-   for (let j = 0; j < headers.length; j++) {
-     tarr.push(data[j]);
-   }
-   //Pusd headings to array variable
-   
-
-   this.lines.push(tarr);
-   
-  
-   // Table Rows
-   let tarrR: string[] | null | any = [];
-   let arrl = allTextLines.length;
-   let rows = [];
-   for(let i = 1; i < arrl; i++){
-   rows.push(allTextLines[i].split(';'));
-  }
-   
-   for (let j = 0; j < arrl; j++) {
-
-       tarrR.push(rows[j]);
-       console.log(rows[j])
-       
-   }
-  //Push rows to array variable
-   this.linesR.push(tarrR);
-           
-}
-}
-}
-
- 
-}
+    fileReader.onload = (e) => {  //LO DIVIDIMOS Y GUARDAMOS EN UN ARRAY LLAMADO "ALL TEXT LINES"
+          let csv: any = fileReader.result;
+          let allTextLines = []; //DECLARAS UN ARREGLO
+          allTextLines = csv.split(/\n/); //guardas el csv en el arreglo, partiendolo cada salto de linea
+                
+          //eliminamos la ultima posicion del array, que es un salto de línea (vacío) 
+          let longitud = allTextLines.length;
+          allTextLines.splice(longitud-1, 1 );
+          longitud = allTextLines.length;
 
 
- /*
-      
-     
-   if(file && files.length > 0) {
-        console.log(file.name);
-        console.log(file.size);
-        console.log(file.type);
-        //File reader method
-        let reader: FileReader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = (e) => {
-         let csv: any = reader.result;
-         let allTextLines = [];
-         allTextLines = csv.split(/\r|\n|\r/);
-        
-        //Table Headings
-         let headers = allTextLines[0].split(';');
-         let data = headers;
-         let tarr: string[] | null | any = [];
-         
-         for (let j = 0; j < headers.length; j++) {
-           tarr.push(data[j]);
-         }
-         //Pusd headings to array variable
-         
+          //IMPRIMIMOS EL ARRAY EN LA CONSOLA
+          for(let i = 0; i < longitud; i++){
+                console.log("la", i, "posicion es", allTextLines[i])   //allTextLines[i].split(';')
+          }
 
-         this.lines.push(tarr);
-         
-        
-         // Table Rows
-         let tarrR: string[] | null | any = [];
-         let arrl = allTextLines.length;
-         let rows = [];
-         for(let i = 1; i < arrl; i++){
-         rows.push(allTextLines[i].split(';'));
         }
-         
-         for (let j = 0; j < arrl; j++) {
-     
-             tarrR.push(rows[j]);
-             console.log(rows[j])
-             
-         }
-        //Push rows to array variable
-         this.linesR.push(tarrR);
-                 
-     }
-   }
- }
 
- 
+          fileReader.readAsText(this.file);
+
+  }
 }
-*/
-
-
-
-
-
-
-
-
-/*
-      console.log(files);
   
-             
- 
-         let file: File = files.item[0] as File;
-     
-   if(file && files.length > 0) {
-        console.log(file.name);
-        console.log(file.size);
-        console.log(file.type);
-        //File reader method
-        let reader: FileReader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = (e) => {
-         let csv: any = reader.result;
-         let allTextLines = [];
-         allTextLines = csv.split(/\r|\n|\r/);
-        
-        //Table Headings
-         let headers = allTextLines[0].split(';');
-         let data = headers;
-         let tarr: string[] | null | any = [];
-         
-         for (let j = 0; j < headers.length; j++) {
-           tarr.push(data[j]);
-         }
-         //Pusd headings to array variable
-         
-
-         this.lines.push(tarr);
-         
-        
-         // Table Rows
-         let tarrR: string[] | null | any = [];
-         let arrl = allTextLines.length;
-         let rows = [];
-         for(let i = 1; i < arrl; i++){
-         rows.push(allTextLines[i].split(';'));
-        }
-         
-         for (let j = 0; j < arrl; j++) {
-     
-             tarrR.push(rows[j]);
-             console.log(rows[j])
-             
-         }
-        //Push rows to array variable
-         this.linesR.push(tarrR);
-                 
-     }
-   }
- }
-
- 
-}
-*/
+  
